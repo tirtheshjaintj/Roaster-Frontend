63ef83300e047fbbc6ffdb19bc4d688859ae4b68
tj <tirtheshjaintj@gmail.com>
2024-07-08 17:38:02 +0530

    AI Roaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { toast, Toaster } from "react-hot-toast";
 import RoastDisplay from "./components/RoastDisplay";
